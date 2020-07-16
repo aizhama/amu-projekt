b0436ffb02738f931ef1790e4dc5b55ac2174aed
aizhama <aijan5416@gmail.com>
2020-07-16 10:50:54 +0200

    AMU TaskPlanner

--- src/main/java/de/amuprojekt/amuprojekt/AmuProjektApplication.java
+++ src/main/java/de/amuprojekt/amuprojekt/AmuProjektApplication.java
@@ -1,4 +1,4 @@
-package de.amuprojekt.amuprojekt.repository;
+package de.amuprojekt.amuprojekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/de/amuprojekt/amuprojekt/controller/MyController.java
+++ src/main/java/de/amuprojekt/amuprojekt/controller/MyController.java
@@ -8,13 +8,10 @@ import de.amuprojekt.amuprojekt.model.Task;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@Controller(value = "/")
-class MyController {
+@Controller
+public class MyController {
     private static List<Task> task = new ArrayList<Task>();
 
     static {
@@ -39,6 +36,7 @@ class MyController {
 
     @RequestMapping(value={"/", "/index"}, method = RequestMethod.GET)
     public String index(Model model) {
+
         model.addAttribute("message", message);
         return "index";
     }
